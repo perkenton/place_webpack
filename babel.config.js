1f4992093497dcb75a5dd26df4d81eab80fe81e0
Alexander Vasiliev <selado@ya.ru>
2020-07-12 18:22:26 +0300

    fix: first iteration's remarks

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,29 @@ const presets = [
   [
     "@babel/env",
     {
-      targets: { // указать цели, для полифилов
+      targets: {
         edge: "17",
         ie: "11",
         firefox: "50",
         chrome: "64",
         safari: "11.1",
       },
-      useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
-      corejs: "3.4.1" // явно проставить версию corejs
+      useBuiltIns: "usage",
+      corejs: "3.4.1"
     }
   ],
 ];
 
+const plugins = [
+  [
+    "@babel/plugin-proposal-class-properties",
+    { 
+      "loose": true 
+    }
+  ]
+];
+
 module.exports = {
-  "plugins": ["@babel/plugin-proposal-class-properties"]
+  presets,
+  plugins
 };
--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,27 @@ module.exports = {
           'file-loader?name=./images/[name].[ext]&esModule=false',
           {
             loader: 'image-webpack-loader',
-            options: {}
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
           },
         ]
       }
